fe7e8397543d388ba48ccbc844dd6e428a3cb2df
Egor Bagrov <e.bagrov@globus-ltd.com>
2017-03-29 17:16:18 +0300

    Task_02

---
+++ 02_Task/server.js
@@ -0,0 +1,23 @@
+
+var express = require('express')
+    , app = express()
+    , port = 3000;
+var bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.all('/api', function(request, response){
+    var json = request.body;
+    var mult = json.a*json.b;
+    response.send(JSON.stringify({ mult: mult }));
+});
+
+app.get('/',function(req,res){
+    res.sendFile(__dirname + '/index.html');
+
+});
+
+app.listen(port, function () {
+    console.log('Listening on port ', port)
+});
