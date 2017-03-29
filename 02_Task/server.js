aecbcb83d29ea96ba2443d2a8f1cabc8f536c802
Egor Bagrov <e.bagrov@globus-ltd.com>
2017-03-29 17:23:26 +0300

    Task_02

--- 02_Task/server.js
+++ 02_Task/server.js
@@ -1,7 +1,6 @@
-
 var express = require('express')
     , app = express()
-    , port = 3000;
+    , port = process.env.PORT || 3000;
 var bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
