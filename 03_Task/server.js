4e1c69639fc5561d5c54911eadf39ca49993f154
Egor Bagrov <e.bagrov@globus-ltd.com>
2017-04-03 22:55:18 +0300

    Task_02

--- 03_Task/server.js
+++ 03_Task/server.js
@@ -1,6 +1,6 @@
 var express = require('express')
     , app = express()
-    , port = process.env.PORT || 3000;
+    , port = 3000;
 const assert = require('assert');
 
 app.get('/api/a/:a/b/:b',function(req,res){
